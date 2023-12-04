a8c0ba06e29b0fc4d0d2bf7a534fb1a266670a3f
Eric <ethai1@cps.edu>
2023-12-04 11:31:07 -0600

    working on a6

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -5,18 +5,41 @@ from sklearn.model_selection import train_test_split
 
 #imports and formats the data
 data = pd.read_csv("part3-multivariable-linear-regression/car_data.csv")
-x = data[["miles","age"]].values
+x = data[["miles(000)","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
 
+coefficent = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y), 2)
+
+print(f"Miles(000) coefficient is: {coefficent[0]}")
+print(f"Age coefficient is: {coefficent[1]}")
+print(f"Intercept is: {intercept}")
+print(f"R_squared value is: {r_squared}")
+print(f"Model equation is y = {coefficent[0]}x1 + {coefficent[1]}x2 + {intercept}")
+
+prediction = model.predict(xtest)
+
+prediction = np.around(prediction, 2)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
 print("***************")
 print("Testing Results")
+
+for num in range(len(xtest)):
+    actual_value = ytest[num]
+    predicted_y = prediction[num]
+    x_coordinate = xtest[num]
+
+    print(f"The miles(000) is: {x_coordinate[0]}, Age is: {x_coordinate[1]}, Predicted Price: ${predicted_y}, Actual Price: ${actual_value}")
+
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,13 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
+The R_squared value is 0.85. This shows me that the miles(000) is well correlated with the price of the car.
 
 2. Is your model accurate? Why or why not?
+The model is accurate because the R_squared value is high which shows that the data is well correlated with the two variables.
+
 
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
+
 
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
